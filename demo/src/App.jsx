ac609ed52a17cf580c1fe769e83028857d41b452
Your Name <archsakpal@gmail.com>
2025-08-30 20:19:56 +0800

    Initial commit: Added greeting function

---
+++ demo/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h2>Welcome to my app</h2>
+     <h3>Docker react demo</h3>
+    </>
+  )
+}
+
+export default App
---
+++ reactdocker/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is my Docker Container App</h1>
+      <p>This is Assessment project</p>
+    </>
+  )
+}
+
+export default App
