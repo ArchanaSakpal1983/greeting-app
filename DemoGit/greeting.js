33d4a1fdd33a08a15fd2664710e25dbc13eceae0
Your Name <archsakpal@gmail.com>
2025-08-30 20:26:11 +0800

    Added Spanish greeting support

--- DemoGit/greeting.js
+++ DemoGit/greeting.js
@@ -1,5 +1,9 @@
-function greet(name) {
+function greet(name, lang = "en") {
+  if (lang === "es") {
+    return `Hola, ${name}!`;
+  }
   return `Hello, ${name}!`;
 }
 
 console.log(greet("World"));
+console.log(greet("Mundo", "es"));
